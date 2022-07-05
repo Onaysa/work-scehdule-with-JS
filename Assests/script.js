5d5b74ffacd32d015f825ed080dcd6f7c1d2c596
Onaysa Ali <anisa.ali.5999385@gmail.com>
2022-07-05 11:41:16 -0400

    save changes

---
+++ Assests/script.js
@@ -0,0 +1,15 @@
+//step 1: put current date on web page using moment()
+//step 2: compare the current hour with time-blocks
+//ste  3: translate time-blocks to military times
+//step 4: use the css classes for present, past or future
+//compare the time-blocks with moment()
+// if moment() > time-blocks =>past
+// if moment() == time-block => Present
+// if  moment() < time-blokcs => future
+//step 4: grap the textarea value and store it in localstorage
+//create a selector for buttons to loop through so when you click on the button you get the value
+//button and Id and textbox are the same, when you click the button need to find out from 0 to 7 which button is clicked 
+// in the function it will have a for loop
+// event.target.Id 
+// add event to all button 1. first thing to do, then pass a function, set this as var to know the id of the button you clicked
+//
